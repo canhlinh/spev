d817b2f9b615935164bfbc6db63e5ed82878345a
LinhNC <linhnc@linhnc-System-Product-Name>
2015-10-29 23:27:30 +0700

    add word translate handler

--- chrome_extension/vndict/background.js
+++ chrome_extension/vndict/background.js
@@ -1,26 +1,55 @@
-chrome.runtime.onConnect.addListener(onPortConnected);
-chrome.runtime.onConnectExternal.addListener(onPortConnected);
 function onPortConnected(port){
-	self.contentScriptPort = port;
+	contentScriptPort = port;
 	port.onDisconnect.addListener(function (){
-		self.contentScriptPort = null;
-		if (self.nativeHostPort){
-			self.onNMHostDisconnected("");
-		}
+		contentScriptPort = null;
 	});
-	port.onMessage.addListener(function (message){
-		var functionToCall = self[message.command];
-		if (functionToCall === undefined || functionToCall === null){
-			self.errorToContentScript("Extension received unknown message: " + JSON.stringify(message));
-			return;
-		}
-		functionToCall(message);
-	});
+	port.onMessage.addListener(messageContentHandler);
+  sendMessageToContent("Message from extension : Hey boss, i'm ready");
 }
 
-this.messageToContentScript = function(cookie, name, data){
-	if (null === self.contentScriptPort){
+function sendMessageToContent(message){
+	if (self.contentScriptPort === null){
 	  return;
 	}
-	self.contentScriptPort.postMessage({ 'cookie': cookie, 'command': name, 'data': data });
-};
+	contentScriptPort.postMessage(message);
+}
+
+function messageContentHandler(event){
+  console.log(event);
+  var mName = event.name;
+  switch(mName){
+    case TRANSLATE_WORD:
+      getWordFromServer(event.data, responseWordTranslateHandler);
+      break;
+  }
+}
+
+function responseWordTranslateHandler (word, result) {
+  var message = {};
+  message.name = WORD_TRANSLATED;
+  message.data = {};
+  message.data.result = result;
+  message.data.word = word;
+  sendMessageToContent(message);
+}
+
+function getWordFromServer(word){
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.onreadystatechange = function()
+  {
+      if (xhr.readyState == 4 && xhr.status == 200)
+      {
+        if(this.response !== null){
+          responseWordTranslateHandler(word, this.response.result);
+        }
+      }
+  }; 
+  xhr.open("GET", "http://vnstreaming.com/vndict/api/searchEV?word="+word);
+  xhr.send();
+}
+
+var contentScriptPort = {};
+
+chrome.runtime.onConnect.addListener(onPortConnected);
+chrome.runtime.onConnectExternal.addListener(onPortConnected);
--- chrome_extension/vndict/script-content.js
+++ chrome_extension/vndict/script-content.js
@@ -1,18 +1,38 @@
-function tryConnectExtension(cookie, extensionId){
-    extension.id = extensionId;
+function tryConnectExtension(){
     extension.port = chrome.runtime.connect(extension.id);
-    if (null === extension.port){
-        sendErrorToWebPage("Cannot connect to extension with ID: " + extension.id);
+    if (extension.port === null){
         extension.id = null;
         extension.port = null;
         return false;
     }
+    console.log("connected to extension : "+extension.id);
+    sendToExtension("message from content");
     extension.port.onMessage.addListener(onExtensionMessage);
     extension.port.onDisconnect.addListener(onExtensionDisconnected);
-    sendToWebPage({ cookie: cookie,
-                    command: "EXTENSION_CONNECTION_CHANGE",
-                    data: { "extensionID": extension.id, "isConnected": true }});
     return true;
+}
+
+function onExtensionMessage(event){
+  console.log(event);
+  switch(event.name){
+    case CONNECTION_CHANGE:
+      if(event.data === "ready")
+        console.log("extension ready");
+      break;
+    case WORD_TRANSLATED:
+      wordTranslatedHandler();
+      break;
+  }
+}
+
+function wordTranslatedHandler(){
+  //Do something
+}
+
+function onExtensionDisconnected(){
+  console.log(event);
+  if(extension.id !== undefined)
+    setTimeout(tryConnectExtension, 3000);
 }
 
 function sendToExtension(message){
@@ -21,4 +41,60 @@ function sendToExtension(message){
     } else{
         sendErrorToWebPage("Extension is not connected. Message cannot be sent.");
     }
+}
+
+function initContentScript(){
+  document.body.onclick = mouseClickHandler;
+  document.body.ondblclick = mouseDbclickHandler;
+  tryConnectExtension();
+}
+
+function mouseClickHandler(){
+  if(clickTimeout === 0)
+    clickTimeout = setTimeout(releaseUI,250);
+}
+
+function releaseUI(){
+  console.log("releaseUI");
+}
+
+function mouseDbclickHandler(){
+  console.log("mouseDbclickHandler");
+  clearTimeout(clickTimeout);
+  clickTimeout = 0;
+  var lookupWord = getSelectedText();
+  lookupWord = lookupWord.replace(/[\.\*\?;!()\+,\[:\]<>^_`\[\]{}~\\\/\"\'=]/g, " ");
+  lookupWord = lookupWord.replace(/\s+/g, " ");
+  if (lookupWord !== null) {
+    console.log(lookupWord);
+    var message = {};
+    message.name = TRANSLATE_WORD;
+    message.data = lookupWord;
+    sendToExtension(message);
+  }
+}
+
+function getSelectedText(){
+    if(window.getSelection)
+        return window.getSelection().toString();
+    else if(document.getSelection)
+        return document.getSelection();
+    else if(document.selection)
+        return document.selection.createRange().text;
+    return "";
+}
+
+var clickTimeout = 0;
+var extension = {};
+
+if(!document.head.querySelector("meta[name=vnstreamg-com-chrome-extension]")){
+  if (chrome.extension) {
+    var e = chrome.extension.getURL("");
+    extension.id = /(\w{32})/.exec(e)[0];
+    var t = window.document.createElement("meta");
+    t.setAttribute("name", "eMx-chrome-extension");
+    t.setAttribute("id", extension.id);
+    window.document.head.appendChild(t);
+    initContentScript();
+  }
 }
