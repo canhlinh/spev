92b9f6376c6f40c968127c1b2cd49eed67335f06
canhlinh <linhnc@linhnc-System-Product-Name>
2015-11-08 14:55:10 +0700

    add mindmap and make a new coding method

--- chrome_extension/vndict/js/note.js
+++ chrome_extension/vndict/js/note.js
@@ -1,4 +1,4 @@
-function Note(){
+function Card(){
   this.word = null;
   this.phonetic = null;
   this.meanings = null;
@@ -27,23 +27,20 @@ function Note(){
   this.ui.onmouseleave = this.hideAddButton;
   this.ui.note = this;
   document.body.appendChild(this.ui);
-  noteList.push(this);
 }
 
-Note.prototype.getUI= function(){
+Card.prototype.getUI= function(){
   return this.ui;
 };
 
-Note.prototype.showAddButton = function(event){
+Card.prototype.showAddButton = function(event){
    var target = event.target ? event.target : event.srcElement;
   if(target.tagName == "DIV" ){
     target.children[0].style.display = "block";
-    if(noteList.length > 1)
-    target.children[1].style.display = "block";
   }
 };
 
-Note.prototype.hideAddButton = function(){
+Card.prototype.hideAddButton = function(){
    var target = event.target ? event.target : event.srcElement;
   if(target.tagName == "DIV" ){
     target.children[0].style.display = "none";
@@ -51,35 +48,62 @@ Note.prototype.hideAddButton = function(){
   }
 };
 
-Note.prototype.deleteNote = function(event){
+Card.prototype.deleteNote = function(event){
   var target = event.target ? event.target : event.srcElement;
   if(target.tagName == "BUTTON" ){
     document.body.removeChild(target.parentElement);
   }
 };
 
-Note.prototype.add = function(){
-  this.nextNote = new Note();
+Card.prototype.add = function(){
+  this.nextNote = new Card();
 };
 
-Note.prototype.save = function(value){
+Card.prototype.save = function(value){
   
 };
 
-Note.prototype.overturn = function(){
+Card.prototype.overturn = function(){
   
 };
 
-Note.prototype.mouseOver = function(){
+Card.prototype.mouseOver = function(){
   
 };
 
-Note.prototype.mouseOut = function(){
+Card.prototype.mouseOut = function(){
   
 };
+
+Object.defineProperty(this,"CardManager",{
+  writable: false,
+  value: Object.freeze( {
+    cards: [],
+    Init: function(){
+      var card = new Card();
+      this.AddCard(card);
+    },
+    CheckLogin: function(){
+      
+    },
+    AddCard: function(card){
+      this.cards.push(card);
+      this.UpdateCard();
+    },
+    DelCard: function(){
+      
+    },
+    UpdateCard: function(){
+      console.log("number of card: "+this.cards.length);
+    },
+    UIprocessor: function(){
+      
+    }
+  })
+});
 
 window.onload = init;
 window.noteList = [];
 function init(){
-  new Note();
+  CardManager.Init();
 }
